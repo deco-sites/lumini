d0c49092610ae02d857c6bd7c4a8867e713691ec
@yuri_assuncx <killerkoll2012@gmail.com>
2023-10-07 00:03:11 -0300

    feat (Benefits): adding Slider on Mobile

--- sections/Content/Benefits.tsx
+++ sections/Content/Benefits.tsx
@@ -1,4 +1,7 @@
 import Icon, { AvailableIcons } from "$store/components/ui/Icon.tsx";
+import Slider from "$store/components/ui/Slider.tsx";
+import SliderJS from "$store/islands/SliderJS.tsx";
+import { useId } from "$store/sdk/useId.ts";
 import Header from "$store/components/ui/SectionHeader.tsx";
 
 export interface Props {
@@ -10,7 +13,7 @@ export interface Props {
     description?: string;
   }>;
   layout?: {
-    variation?: "Simple" | "With border" | "Color reverse";
+    variation?: "Simple" | "With border" | "Color reverse" | "Slider";
     headerAlignment?: "center" | "left";
   };
 }
@@ -36,6 +39,8 @@ export default function Benefits(
     }],
     layout,
   } = props;
+
+  const id = useId();
 
   const listOfBenefits = benefits.map((benefit, index) => {
     // const showDivider = index < benefits.length - 1;
@@ -134,6 +139,38 @@ export default function Benefits(
             <div class="grid grid-cols-2 gap-4 w-full lg:gap-8 lg:grid-flow-col lg:auto-cols-fr">
               {listOfBenefits}
             </div>
+          </div>
+        </div>
+      )}
+      {layout?.variation === "Slider" && (
+        <div class="w-full px-4 py-2.5 flex flex-col items-center justify-center gap-8">
+          <Header
+            title={title}
+            description={description}
+            alignment={layout?.headerAlignment || "center"}
+          />
+          <div id={id} class="w-full flex items-center justify-center">
+            <Slider class="carousel carousel-center w-full">
+              {benefits?.map((benefit, index) => (
+                <Slider.Item
+                  index={index}
+                  class="carousel-item w-full items-center justify-center gap-1.5"
+                >
+                  <Icon
+                    id={benefit.icon}
+                    class={"text-primary"}
+                    width={36}
+                    height={36}
+                    strokeWidth={0.01}
+                    fill="currentColor"
+                  />
+                  <span class="text-base leading-7 text-base-content">
+                    {benefit.label}
+                  </span>
+                </Slider.Item>
+              ))}
+            </Slider>
+            <SliderJS rootId={id} infinite scroll="smooth" interval={6 * 1e3} />
           </div>
         </div>
       )}
