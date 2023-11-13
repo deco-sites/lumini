2ba815ab3f03ee5fad0dc7baab63f8296673a329
@yuri_assuncx <killerkoll2012@gmail.com>
2023-11-13 17:08:59 -0300

    fix: CollecitonSection modified

--- components/ui/CollectionSection.tsx
+++ components/ui/CollectionSection.tsx
@@ -7,13 +7,15 @@ export interface Props {
     image: ImageWidget;
     link: string;
     description: string;
+    width?: number;
+    height?: number;
   }[];
 }
 
 export default function CollectionSection({ title, images }: Props) {
   return (
     <div class="w-full h-full flex items-center justify-center">
-      <section class="max-w-full lg:max-w-[1536px] flex items-center justify-center m-5">
+      <section class="max-w-full lg:max-w-[1230px] flex items-center justify-center m-5">
         <div class="w-full h-full flex flex-col items-center justify-center">
           <div class="w-full flex items-start justify-start my-5">
             <h1 class="text-[21px] lg:text-4xl font-univers-next-pro-bold">
@@ -25,8 +27,8 @@ export default function CollectionSection({ title, images }: Props) {
               <a href={item.link} class="w-full h-full">
                 <Image
                   src={item.image}
-                  width={615}
-                  height={637}
+                  width={item.width ?? 615}
+                  height={item.height ?? 637}
                   alt={item.description}
                   loading="lazy"
                 />
