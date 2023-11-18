8c43a8e6b0cbb45e230ccfc808bc25dd995e93f1
@yuri_assuncx <killerkoll2012@gmail.com>
2023-11-18 19:14:07 -0300

    fix: OurStores modified

--- components/ui/OurStores.tsx
+++ components/ui/OurStores.tsx
@@ -4,7 +4,7 @@ import { ImageWidget } from "apps/admin/widgets.ts";
 import { HTMLWidget } from "apps/admin/widgets.ts";
 
 export interface Props {
-  title: HTMLWidget;
+  title: string;
   cards?: {
     image: {
       linkImage: ImageWidget;
@@ -19,15 +19,14 @@ export interface Props {
 
 export default function OurStores({ title, cards }: Props) {
   return (
-    <section class="w-full h-full flex items-center justify-center my-2">
+    <section class="w-full h-full flex items-center justify-center my-2 px-4 xl:px-0">
       <div class="max-w-[1536px] h-full flex flex-col items-start justify-start mx-auto mt-4">
         <div class="w-full h-full my-4">
-          <p
-            dangerouslySetInnerHTML={{ __html: title ?? "" }}
-            class="text-[40px] text-[#3f3f40] my-4"
-          />
+          <p class="text-xl lg:text-[40px] text-[#3f3f40] my-4 font-semibold">
+            {title}
+          </p>
         </div>
-        <div class="w-full h-full flex flex-wrap items-center justify-start gap-4">
+        <div class="w-full h-full grid sm:grid-cols-2 md:grid-cols-3 items-center justify-center md:justify-start gap-4">
           {cards?.map((card) => (
             <div class="max-w-[450px] w-full h-full flex flex-col">
               <div class="w-full h-full">
@@ -49,11 +48,11 @@ export default function OurStores({ title, cards }: Props) {
                   class="text-base my-2"
                 />
               </div>
-              <a href={card.linkButton} class="w-full flex items-center">
-                <button class="flex items-center bg-transparent hover:bg-[#1d1d1b] text-[#1d1d1b] font-semibold hover:text-white py-2 px-4 border border-[#1d1d1b] hover:border-transparent my-4">
+              <a href={card.linkButton} class="w-full flex items-center group">
+                <button class="flex items-center bg-transparent group-hover:bg-[#1d1d1b] text-[#1d1d1b] group-hover:text-white py-2 px-4 border border-[#1d1d1b] group-hover:border-transparent my-4">
                   {card.titleButton ?? "ver no mapa"}
                   <Icon
-                    class="text-black"
+                    class="text-black group-hover:text-white"
                     size={28}
                     id="ChevronRight"
                     strokeWidth={1}
