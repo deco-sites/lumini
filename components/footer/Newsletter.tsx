4de956a0265b325e9cbba4e48d1cffe81bc90c18
@yuri_assuncx <killerkoll2012@gmail.com>
2023-11-18 22:47:20 -0300

    fix: UI/UX improved

--- components/footer/Newsletter.tsx
+++ components/footer/Newsletter.tsx
@@ -26,6 +26,7 @@ function Newsletter(
 ) {
   const { tiled = false } = layout;
   const loading = useSignal(false);
+  const isSubmitted = useSignal(false);
 
   const handleSubmit: JSX.GenericEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
@@ -39,6 +40,7 @@ function Newsletter(
       await invoke.vtex.actions.newsletter.subscribe({ email });
     } finally {
       loading.value = false;
+      isSubmitted.value = true;
     }
   };
 
@@ -59,32 +61,34 @@ function Newsletter(
         {content?.description && <div>{content?.description}</div>}
       </div>
       <div class="flex flex-col gap-4">
-        <form
-          class="flex items-center justify-center w-full border-b border-b-black pb-0.5 h-[46px]"
-          onSubmit={handleSubmit}
-        >
-          <div class="flex justify-between items-center w-full h-full">
-            <input
-              name="email"
-              class="flex w-full text-base-content bg-transparent pl-1.5 focus:outline-none h-full"
-              placeholder={content?.form?.placeholder || "insira seu email"}
-            />
-
-            <button
-              type="submit"
-              class="disabled:loading pr-1.5 h-full"
-              disabled={loading}
-            >
-              <img
-                alt="right arrow image"
-                width={24}
-                height={24}
-                loading="lazy"
-                src="https://tezexb.vtexassets.com/assets/vtex/assets-builder/tezexb.lumini-store-theme/1.9.3/svg/hpa-newsletter-row___ef97774d74950b40c21bc4b0ba8ffec1.svg"
+        {isSubmitted.value ? <p>obrigado!</p> : (
+          <form
+            class="flex items-center justify-center w-[90%] sm:w-full border-b border-b-black pb-0.5 h-[46px]"
+            onSubmit={handleSubmit}
+          >
+            <div class="flex justify-between items-center w-full h-full">
+              <input
+                name="email"
+                class="flex w-full text-base-content bg-transparent pl-1.5 focus:outline-none h-full"
+                placeholder={content?.form?.placeholder || "insira seu email"}
               />
-            </button>
-          </div>
-        </form>
+
+              <button
+                type="submit"
+                class="disabled:loading pr-1.5 h-full"
+                disabled={loading}
+              >
+                <img
+                  alt="right arrow image"
+                  width={24}
+                  height={24}
+                  loading="lazy"
+                  src="https://tezexb.vtexassets.com/assets/vtex/assets-builder/tezexb.lumini-store-theme/1.9.3/svg/hpa-newsletter-row___ef97774d74950b40c21bc4b0ba8ffec1.svg"
+                />
+              </button>
+            </div>
+          </form>
+        )}
         {
           /* {content?.form?.helpText && (
           <div
--- components/ui/AboutSection.tsx
+++ components/ui/AboutSection.tsx
@@ -16,9 +16,9 @@ export default function AboutSection(
   { title, subTitle, titleButton, linkButton, image }: Props,
 ) {
   return (
-    <section class="max-w-[1536px] flex items-center justify-center mx-auto">
-      <div class="w-full h-full flex flex-col md:flex-row items-center justify-center ">
-        <div class="md:w-2/4 h-full flex flex-col ml-[145px] mr-[102px]">
+    <section class="max-w-[1250px] flex items-center justify-center mx-auto px-2 lg:px-0">
+      <div class="w-full h-full flex flex-col md:flex-row items-center justify-center gap-3">
+        <div class="max-w-[540px] w-full h-full flex flex-col">
           <h1 dangerouslySetInnerHTML={{ __html: title ?? "" }} />
           <p dangerouslySetInnerHTML={{ __html: subTitle ?? "" }} />
           <div class="my-8">
@@ -30,7 +30,7 @@ export default function AboutSection(
           </div>
         </div>
         {image && (
-          <div class="md:w-2/4 h-full mt-[-14px]">
+          <div class="max-w-[500px] w-full h-full mt-[-14px]">
             <Image
               src={image.linkImage}
               width={500}
