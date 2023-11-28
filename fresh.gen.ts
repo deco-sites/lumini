ede45ea1b290a26d87e170c6a16d02cff44bcb93
@yuri_assuncx <killerkoll2012@gmail.com>
2023-11-27 23:00:36 -0300

    chore: versions updated

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -22,9 +22,10 @@ import * as $$15 from "./islands/Popup.tsx";
 import * as $$16 from "./islands/ProductImageZoom.tsx";
 import * as $$17 from "./islands/ProductVariations.tsx";
 import * as $$18 from "./islands/SearchControls.tsx";
-import * as $$19 from "./islands/ShippingSimulation.tsx";
-import * as $$20 from "./islands/SliderJS.tsx";
-import * as $$21 from "./islands/WishlistButton.tsx";
+import * as $$19 from "./islands/SectionDescription.tsx";
+import * as $$20 from "./islands/ShippingSimulation.tsx";
+import * as $$21 from "./islands/SliderJS.tsx";
+import * as $$22 from "./islands/WishlistButton.tsx";
 
 const manifest = {
   routes: {
@@ -50,9 +51,10 @@ const manifest = {
     "./islands/ProductImageZoom.tsx": $$16,
     "./islands/ProductVariations.tsx": $$17,
     "./islands/SearchControls.tsx": $$18,
-    "./islands/ShippingSimulation.tsx": $$19,
-    "./islands/SliderJS.tsx": $$20,
-    "./islands/WishlistButton.tsx": $$21,
+    "./islands/SectionDescription.tsx": $$19,
+    "./islands/ShippingSimulation.tsx": $$20,
+    "./islands/SliderJS.tsx": $$21,
+    "./islands/WishlistButton.tsx": $$22,
   },
   baseUrl: import.meta.url,
 };
