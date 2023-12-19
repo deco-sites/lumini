d8d3f0563862b01a0d25854613349fe7d5dda2cd
@yuri_assuncx <killerkoll2012@gmail.com>
2023-12-19 17:42:20 -0300

    fix (PDP): image zoom improved

--- islands/ProductImage.tsx
+++ islands/ProductImage.tsx
@@ -1,5 +1,5 @@
 import { FunctionComponent, h } from "preact";
-import { useState } from "preact/compat";
+import { useEffect, useState } from "preact/hooks";
 
 import Slider from "$store/components/ui/Slider.tsx";
 import Image from "apps/website/components/Image.tsx";
@@ -13,10 +13,41 @@ const WIDTH = 709;
 const HEIGHT = 709;
 const ASPECT_RATIO = `${WIDTH} / ${HEIGHT}`;
 
+// ...
+
 const ProductImage: FunctionComponent<{ images: ImageProps[] }> = (
   { images },
 ) => {
   const [isZoomed, setIsZoomed] = useState(false);
+  const [mouseMoveListener, setMouseMoveListener] = useState<
+    ((e: MouseEvent) => void) | null
+  >(null);
+
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      const image = e.currentTarget as HTMLImageElement;
+      const boundingRect = image.getBoundingClientRect();
+      const mouseX = e.clientX - boundingRect.left;
+      const mouseY = e.clientY - boundingRect.top;
+
+      const percentageX = (mouseX / boundingRect.width) * 100;
+      const percentageY = (mouseY / boundingRect.height) * 100;
+
+      image.style.transform = `scale(1.8)`;
+      image.style.transformOrigin = `${percentageX}% ${percentageY}%`;
+      image.style.zIndex = "20";
+    };
+
+    if (isZoomed) {
+      setMouseMoveListener(() => handleMouseMove);
+    } else {
+      setMouseMoveListener(null);
+    }
+
+    return () => {
+      setMouseMoveListener(null);
+    };
+  }, [isZoomed]);
 
   const handleImageClick = (
     event: h.JSX.TargetedMouseEvent<HTMLImageElement>,
@@ -26,61 +57,43 @@ const ProductImage: FunctionComponent<{ images: ImageProps[] }> = (
     const image = event.currentTarget;
 
     if (isZoomed) {
-      // Se a imagem já estiver com zoom, remover o zoom
       image.style.transform = "scale(1)";
       image.style.transformOrigin = "center center";
       image.style.zIndex = "0";
       setIsZoomed(false);
     } else {
-      // Se a imagem não estiver com zoom, adicionar o zoom
-      const handleMouseMove = (e: MouseEvent) => {
-        const boundingRect = image.getBoundingClientRect();
-        const mouseX = e.clientX - boundingRect.left;
-        const mouseY = e.clientY - boundingRect.top;
-
-        const percentageX = (mouseX / boundingRect.width) * 100;
-        const percentageY = (mouseY / boundingRect.height) * 100;
-
-        image.style.transform = `scale(1.8)`;
-        image.style.transformOrigin = `${percentageX}% ${percentageY}%`;
-        image.style.zIndex = "20";
-      };
-
-      // Remover o zoom e os event listeners quando o mouse sai da imagem
-      const handleMouseLeave = () => {
-        image.style.transform = "scale(1)";
-        image.style.transformOrigin = "center center";
-        image.style.zIndex = "0";
-        setIsZoomed(false);
-
-        image.removeEventListener("mousemove", handleMouseMove);
-        image.removeEventListener("mouseleave", handleMouseLeave);
-      };
-
-      // Adicionar o zoom e os event listeners
       setIsZoomed(true);
-      image.addEventListener("mousemove", handleMouseMove);
-      image.addEventListener("mouseleave", handleMouseLeave);
     }
   };
 
   return (
     <Slider class="carousel carousel-center gap-6 w-screen sm:w-full">
       {images.map((img, index) => (
-        <Slider.Item
-          index={index}
-          class="carousel-item w-full"
-          key={index}
-        >
+        <Slider.Item index={index} class="carousel-item w-full" key={index}>
           <Image
-            class="w-full hover:cursor-zoom-in"
+            class={`w-full hover:cursor-zoom-in ${
+              isZoomed ? "zoomed-image" : ""
+            }`}
             sizes="(max-width: 640px) 100vw, 40vw"
             style={{ aspectRatio: ASPECT_RATIO }}
             src={img.url!}
             alt={img.alternateName}
             width={WIDTH}
             height={HEIGHT}
-            onClick={(e) => handleImageClick(e)}
+            onClick={(e) =>
+              handleImageClick(e)}
+            onMouseLeave={(e) => {
+              const image = e.currentTarget as HTMLImageElement;
+              image.style.transform = "scale(1)";
+              image.style.transformOrigin = "center center";
+              image.style.zIndex = "0";
+
+              setIsZoomed(false);
+            }}
+            onMouseMove={mouseMoveListener
+              ? (e) =>
+                mouseMoveListener(e)
+              : undefined}
             preload={index === 0}
             loading={index === 0 ? "eager" : "lazy"}
           />
